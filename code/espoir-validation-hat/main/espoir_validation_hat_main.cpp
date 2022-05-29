9c496ec753d9124d08c31d95eab0cc42be3b5d25
ma-lalonde <marc@connaxio.com>
2022-05-29 00:25:32 -0400

    Adapt validation script for rev 0.3.6 Fix inverted SCL/SDA pins on schematics (no functional problems)

--- code/espoir-validation-hat/main/espoir_validation_hat_main.cpp
+++ code/espoir-validation-hat/main/espoir_validation_hat_main.cpp
@@ -4,7 +4,6 @@
  *
  */
 
-
 #include <stdio.h>
 #include <math.h>
 
@@ -36,24 +35,28 @@ TaskHandle_t eth_init_handle;
 TaskHandle_t validation_handle;
 esp_eth_mac_t *mac;
 
-
 // List IO pins as digital or analog. IO18 and IO23 are excluded because they are the I2C pins.
 uint8_t digital_io_count = 9;
-uint8_t digital_io_pins[] = { GPIO_NUM_2, GPIO_NUM_4, GPIO_NUM_5, GPIO_NUM_9, GPIO_NUM_10, GPIO_NUM_12,
-		GPIO_NUM_13, GPIO_NUM_14, GPIO_NUM_15 };
+uint8_t digital_io_pins[] = {
+		GPIO_NUM_2,
+		GPIO_NUM_4,
+		GPIO_NUM_5,
+		GPIO_NUM_9,
+		GPIO_NUM_10,
+		GPIO_NUM_12,
+		GPIO_NUM_13,
+		GPIO_NUM_14,
+		GPIO_NUM_15 };
 uint8_t analog_io_count = 5;
 uint8_t analog_io_pins[] = { GPIO_NUM_34, GPIO_NUM_36, GPIO_NUM_37, GPIO_NUM_38, GPIO_NUM_39 };
 
 // List ADC input pins
-const uint8_t num_channels = 5;
-uint8_t adc1_pins[num_channels] = { 34, 36, 37, 38, 39 };
-uint8_t adc2_pins[num_channels] = { 4, 12, 13, 14, 15 };
+const uint8_t num_channels_adc1 = 5;
+const uint8_t num_channels_adc2 = 6;
+uint8_t adc1_pins[num_channels_adc1] = { 34, 36, 37, 38, 39 };
+uint8_t adc2_pins[num_channels_adc2] = { 2, 4, 12, 13, 14, 15 };
 bool write_two_point_adc_cal = true;
-
-
-
-
-
+esp_err_t err_dhcp = ESP_FAIL;
 
 /** Event handler for Ethernet events */
 static void eth_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
@@ -93,7 +96,7 @@ static void got_ip_event_handler(void *arg, esp_event_base_t event_base, int32_t
 	ESP_LOGI("ETH", "ETHMASK:" IPSTR, IP2STR(&ip_info->netmask));
 	ESP_LOGI("ETH", "ETHGW:" IPSTR, IP2STR(&ip_info->gw));
 	ESP_LOGI("ETH", "~~~~~~~~~~~");
-	printf("Ethernet DHCP:\tESP_OK.\r\n");
+	err_dhcp = ESP_OK;
 }
 
 void ethernet_init(void *pvParameters) {
@@ -145,8 +148,6 @@ void ethernet_init(void *pvParameters) {
 void validate_board(void *pvParameters) {
 	uint8_t settling_ticks = pdMS_TO_TICKS(50);
 
-
-
 	/* Disable all GPIOs for next step */
 	gpio_config_t gpio_conf = { .pin_bit_mask = (1ULL << GPIO_NUM_2), .mode = GPIO_MODE_DISABLE, .pull_up_en =
 			GPIO_PULLUP_DISABLE, .pull_down_en = GPIO_PULLDOWN_DISABLE, .intr_type = GPIO_INTR_DISABLE };
@@ -162,8 +163,8 @@ void validate_board(void *pvParameters) {
 	}
 
 	/* Initialize validation board DAC and ADC */
-	Sensor *adc = new ADS1119("validation_adc", GPIO_NUM_18, GPIO_NUM_23);
-	DACx0501 *dac = new DACx0501("validation_dac", GPIO_NUM_18, GPIO_NUM_23);
+	Sensor *adc = new ADS1119("validation_adc", GPIO_NUM_23, GPIO_NUM_18);
+	DACx0501 *dac = new DACx0501("validation_dac", GPIO_NUM_23, GPIO_NUM_18);
 	esp_err_t err = adc->init();
 	ESP_LOGD("main", "ADC init: %s", esp_err_to_name(err));
 
@@ -217,10 +218,10 @@ void validate_board(void *pvParameters) {
 
 	// Do ADC Two Point calibration
 	if (err == ESP_OK) {
-		uint32_t adc1_readings_150[num_channels] = { 0 };
-		uint32_t adc2_readings_150[num_channels] = { 0 };
-		uint32_t adc1_readings_850[num_channels] = { 0 };
-		uint32_t adc2_readings_850[num_channels] = { 0 };
+		uint32_t adc1_readings_150[num_channels_adc1] = { 0 };
+		uint32_t adc2_readings_150[num_channels_adc2] = { 0 };
+		uint32_t adc1_readings_850[num_channels_adc1] = { 0 };
+		uint32_t adc2_readings_850[num_channels_adc2] = { 0 };
 		float adc_ref_readings[2] = { 0 };
 		float adc_ref_errors[2] = { 0 };
 		uint8_t avg_pow = 5;	// Average readings with n = 2^avg_pow
@@ -236,30 +237,35 @@ void validate_board(void *pvParameters) {
 		}
 		if (err == ESP_OK) {
 			adc_power_acquire();
-			for (uint8_t i = 0; i < num_channels; ++i) {
+			for (uint8_t i = 0; i < num_channels_adc1; ++i) {
 				ADCDescriptor adc1((gpio_num_t) adc1_pins[i], ADC_WIDTH_BIT_12, ADC_ATTEN_DB_0);
-				ADCDescriptor adc2((gpio_num_t) adc2_pins[i], ADC_WIDTH_BIT_12, ADC_ATTEN_DB_0);
 				for (uint8_t j = 0; j < (1 << avg_pow); ++j) {
 					adc1.update();
-					adc2.update();
 					adc1_readings_150[i] += adc1.getRawValue();
-					adc2_readings_150[i] += adc2.getRawValue();
 				}
 				adc1_readings_150[i] = adc1_readings_150[i] >> avg_pow;
+			}
+			for (uint8_t i = 0; i < num_channels_adc2; ++i) {
+				ADCDescriptor adc2((gpio_num_t) adc2_pins[i], ADC_WIDTH_BIT_12, ADC_ATTEN_DB_0);
+				for (uint8_t j = 0; j < (1 << avg_pow); ++j) {
+					adc2.update();
+					adc2_readings_150[i] += adc2.getRawValue();
+				}
 				adc2_readings_150[i] = adc2_readings_150[i] >> avg_pow;
 			}
+
 			for (uint8_t j = 0; j < (1 << avg_pow); ++j) {
-				static_cast<ADS1119*>(adc)->updateChannel(0);
-				adc_ref_readings[0] += adc->getValue("ch0");
+				static_cast<ADS1119*>(adc)->updateChannel(3);
+				adc_ref_readings[0] += adc->getValue("ch3");
 			}
 			adc_ref_readings[0] /= (1 << avg_pow);
 
 			adc_power_release();
-//            printf("ADC1 readings:\t%i\t%i\t%i\t%i\t%i\r\n", adc1_readings_150[0], adc1_readings_150[1],
-//                    adc1_readings_150[2], adc1_readings_150[3], adc1_readings_150[4]);
-//            printf("ADC2 readings:\t%i\t%i\t%i\t%i\t%i\r\n", adc2_readings_150[0], adc2_readings_150[1],
-//                    adc2_readings_150[2], adc2_readings_150[3], adc2_readings_150[4]);
-//            printf("Ref ADC voltage:\t%.3f V\r\n", adc_ref_readings[0]);
+            printf("ADC1 readings:\t%i\t%i\t%i\t%i\t%i\r\n", adc1_readings_150[0], adc1_readings_150[1],
+                    adc1_readings_150[2], adc1_readings_150[3], adc1_readings_150[4]);
+            printf("ADC2 readings:\t%i\t%i\t%i\t%i\t%i\t%i\r\n", adc2_readings_150[0], adc2_readings_150[1],
+                    adc2_readings_150[2], adc2_readings_150[3], adc2_readings_150[4], adc2_readings_150[5]);
+            printf("Ref ADC voltage:\t%.3f V\r\n", adc_ref_readings[0]);
 			adc_ref_errors[0] = (adc_ref_readings[0] - 150) / 150;
 			printf("Ref ADC 150 mV error:\t%.2f%%\r\n", 100.0f * adc_ref_errors[0]);
 		}
@@ -272,30 +278,35 @@ void validate_board(void *pvParameters) {
 		if (err == ESP_OK) {
 			adc_power_acquire();
 
-			for (uint8_t i = 0; i < num_channels; ++i) {
+			for (uint8_t i = 0; i < num_channels_adc1; ++i) {
 				ADCDescriptor adc1((gpio_num_t) adc1_pins[i], ADC_WIDTH_BIT_12, ADC_ATTEN_DB_0);
-				ADCDescriptor adc2((gpio_num_t) adc2_pins[i], ADC_WIDTH_BIT_12, ADC_ATTEN_DB_0);
 				for (uint8_t j = 0; j < (1 << avg_pow); ++j) {
 					adc1.update();
-					adc2.update();
 					adc1_readings_850[i] += adc1.getRawValue();
-					adc2_readings_850[i] += adc2.getRawValue();
 				}
 				adc1_readings_850[i] = adc1_readings_850[i] >> avg_pow;
+			}
+			for (uint8_t i = 0; i < num_channels_adc2; ++i) {
+				ADCDescriptor adc2((gpio_num_t) adc2_pins[i], ADC_WIDTH_BIT_12, ADC_ATTEN_DB_0);
+				for (uint8_t j = 0; j < (1 << avg_pow); ++j) {
+					adc2.update();
+					adc2_readings_850[i] += adc2.getRawValue();
+				}
 				adc2_readings_850[i] = adc2_readings_850[i] >> avg_pow;
 			}
+
 			for (uint8_t j = 0; j < (1 << avg_pow); ++j) {
-				static_cast<ADS1119*>(adc)->updateChannel(0);
-				adc_ref_readings[1] += adc->getValue("ch0");
+				static_cast<ADS1119*>(adc)->updateChannel(3);
+				adc_ref_readings[1] += adc->getValue("ch3");
 			}
 			adc_ref_readings[1] /= (1 << avg_pow);
 
 			adc_power_release();
-//            printf("ADC1 readings:\t%i\t%i\t%i\t%i\t%i\r\n", adc1_readings_850[0], adc1_readings_850[1],
-//                    adc1_readings_850[2], adc1_readings_850[3], adc1_readings_850[4]);
-//            printf("ADC2 readings:\t%i\t%i\t%i\t%i\t%i\r\n", adc2_readings_850[0], adc2_readings_850[1],
-//                    adc2_readings_850[2], adc2_readings_850[3], adc2_readings_850[4]);
-//            printf("Ref ADC voltage:\t%.3f V\r\n", adc_ref_readings[1]);
+            printf("ADC1 readings:\t%i\t%i\t%i\t%i\t%i\r\n", adc1_readings_850[0], adc1_readings_850[1],
+                    adc1_readings_850[2], adc1_readings_850[3], adc1_readings_850[4]);
+            printf("ADC2 readings:\t%i\t%i\t%i\t%i\t%i\t%i\r\n", adc2_readings_850[0], adc2_readings_850[1],
+                    adc2_readings_850[2], adc2_readings_850[3], adc2_readings_850[4], adc2_readings_850[5]);
+            printf("Ref ADC voltage:\t%.3f V\r\n", adc_ref_readings[1]);
 			adc_ref_errors[1] = (adc_ref_readings[1] - 850) / 850;
 			printf("Ref ADC 850 mV error:\t%.2f%%\r\n", 100.0f * adc_ref_errors[1]);
 		}
@@ -310,27 +321,33 @@ void validate_board(void *pvParameters) {
 			/* Calculate mean and std dev for readings, make sure there are no outliers */
 			float A1_mean = 0, B1_mean = 0, A2_mean = 0, B2_mean = 0;
 			float A1_stdev = 0, B1_stdev = 0, A2_stdev = 0, B2_stdev = 0;
-			for (uint8_t i = 0; i < num_channels; ++i) {
+			for (uint8_t i = 0; i < num_channels_adc1; ++i) {
 				A1_mean += adc1_readings_150[i];
 				B1_mean += adc1_readings_850[i];
+			}
+			for (uint8_t i = 0; i < num_channels_adc2; ++i) {
 				A2_mean += adc2_readings_150[i];
 				B2_mean += adc2_readings_850[i];
 			}
-			A1_mean /= 5.0f;
-			B1_mean /= 5.0f;
-			A2_mean /= 5.0f;
-			B2_mean /= 5.0f;
 
-			for (uint8_t i = 0; i < num_channels; ++i) {
+			A1_mean /= 1.0f * num_channels_adc1;
+			B1_mean /= 1.0f * num_channels_adc1;
+			A2_mean /= 1.0f * num_channels_adc2;
+			B2_mean /= 1.0f * num_channels_adc2;
+
+			for (uint8_t i = 0; i < num_channels_adc1; ++i) {
 				A1_stdev += (adc1_readings_150[i] - A1_mean) * (adc1_readings_150[i] - A1_mean);
 				B1_stdev += (adc1_readings_850[i] - B1_mean) * (adc1_readings_850[i] - B1_mean);
+			}
+			for (uint8_t i = 0; i < num_channels_adc2; ++i) {
 				A2_stdev += (adc2_readings_150[i] - A2_mean) * (adc2_readings_150[i] - A2_mean);
 				B2_stdev += (adc2_readings_850[i] - B2_mean) * (adc2_readings_850[i] - B2_mean);
 			}
-			A1_stdev = sqrt(1.0f * A1_stdev / num_channels);
-			B1_stdev = sqrt(1.0f * B1_stdev / num_channels);
-			A2_stdev = sqrt(1.0f * A2_stdev / num_channels);
-			B2_stdev = sqrt(1.0f * B2_stdev / num_channels);
+
+			A1_stdev = sqrt(1.0f * A1_stdev / num_channels_adc1);
+			B1_stdev = sqrt(1.0f * B1_stdev / num_channels_adc1);
+			A2_stdev = sqrt(1.0f * A2_stdev / num_channels_adc2);
+			B2_stdev = sqrt(1.0f * B2_stdev / num_channels_adc2);
 
 			printf("A1 mean / stdev:\t%.2f / %.2f\r\n", A1_mean, A1_stdev);
 			printf("B1 mean / stdev:\t%.2f / %.2f\r\n", B1_mean, B1_stdev);
@@ -390,16 +407,16 @@ void validate_board(void *pvParameters) {
 			if (err == ESP_OK) {
 				gpio_set_level((gpio_num_t) digital_io_pins[i], 0);
 				vTaskDelay(settling_ticks);
-				err = static_cast<ADS1119*>(adc)->updateChannel(0);
-				value = adc->getValue("ch0");
+				err = static_cast<ADS1119*>(adc)->updateChannel(3);
+				value = adc->getValue("ch3");
 				if (value > 100 || err != ESP_OK) {
 					ESP_LOGI("VALIDATION", "ADC[%i] value: %i. Error:\t%s", i, value, esp_err_to_name(err));
 					err = ESP_ERR_INVALID_RESPONSE;
 				} else {
 					gpio_set_level((gpio_num_t) digital_io_pins[i], 1);
 					vTaskDelay(settling_ticks);
-					err = static_cast<ADS1119*>(adc)->updateChannel(0);
-					value = adc->getValue("ch0");
+					err = static_cast<ADS1119*>(adc)->updateChannel(3);
+					value = adc->getValue("ch3");
 					if (value < 1500 || err != ESP_OK) {
 						ESP_LOGI("VALIDATION", "ADC[%i] value: %i. Error:\t%s", i, value,
 								esp_err_to_name(err));
@@ -483,23 +500,25 @@ void validate_board(void *pvParameters) {
 
 	if (err == ESP_OK) {
 		/* Measure supply voltages */
-		float error_12V = (adc->getValue("ch2") * 11.2 - 12000) / 12000;
-		float error_5V6 = (adc->getValue("ch1") * 6.1 - 5600) / 5600;
-		float error_3V3 = (adc->getValue("ch3") * 6.1 - 3300) / 3300;
+		float error_5V = (adc->getValue("ch1") * 6.1 - 5000) / 5000;
+		float error_3V3 = (adc->getValue("ch0") * 6.1 - 3300) / 3300;
 
-		if (abs(error_12V) > 0.05f || abs(error_5V6) > 0.05f || abs(error_3V3) > 0.01f) {
+		if (abs(error_5V) > 0.02f || abs(error_3V3) > 0.01f) {
 			err = ESP_ERR_INVALID_STATE;
 		}
 
-		printf("12V error:\t%.2f%%\r\n", error_12V * 100.0f);
-		printf("5.6V error:\t%.2f%%\r\n", error_5V6 * 100.0f);
+		printf("5V error:\t%.2f%%\r\n", error_5V * 100.0f);
 		printf("3.3V error:\t%.2f%%\r\n", error_3V3 * 100.0f);
 		printf("Voltages:\t%s\r\n", esp_err_to_name(err));
 	}
 
 	delete (adc);
 	delete (dac);
-	printf("Validation:\t%s.\r\n", esp_err_to_name(err));
+	while (err != ESP_OK || err_dhcp != ESP_OK){
+		printf("I/Os: %s | DHCP: %s\r\n", esp_err_to_name(err), esp_err_to_name(err_dhcp));
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
+	printf("I/Os: %s | DHCP: %s\r\nQC PASS\r\n", esp_err_to_name(err), esp_err_to_name(err_dhcp));
 	vTaskDelete(NULL);
 }
 
